12a14389da863377517905bde85b7eab74651ea9
hannahsvaughan <hannahvaughan421@gmail.com>
2023-02-02 08:18:17 -0500

    inserted values into dep table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,11 @@
 -- SPECIFY DB FOR USE
+USE employees_db;
 
 -- INSERT DATA INTO "DEPARTMENT" TABLE
+INSERT INTO department (name) VALUES ('Sales');
+INSERT INTO department (name) VALUES ('Engineering');
+INSERT INTO department (name) VALUES ('Finance');
+INSERT INTO department (name) VALUES ('Legal');
 
 -- INSERT DATA INTO "ROLE" TABLE
 
