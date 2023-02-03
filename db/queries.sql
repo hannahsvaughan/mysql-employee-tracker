12095ebecd73daecb49175a7a78b733f615367bb
hannahsvaughan <hannahvaughan421@gmail.com>
2023-02-03 09:25:25 -0500

    committing changes made before class starts

--- db/queries.sql
+++ db/queries.sql
@@ -1,5 +1,11 @@
 -- ADD TO DEPARTMENT TABLE
+INSERT INTO department (department_name)
+VALUES (newDepartment)
 
 -- ADD TO ROLE TABLE
+INSERT INTO role (title, salary, department_id)
+VALUES ()
 
 -- ADD TO EMPLOYEE TABLE
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ()
